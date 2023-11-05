918825bdd4e82054bdc0a0944c89fdd84963a427
Alamin-22 <mdalaminmollick@gmail.com>
2023-11-06 03:41:21 +0600

    get api

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+// const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials: true
+}));
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hda1bm.mongodb.net/?retryWrites=true&w=majority`;
